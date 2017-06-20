c2a567b3aa451f1f9a2443f444eec4f034af6eff
root <root@j312208.servers.jiffybox.net>
2017-06-20 19:51:19 +0000

    lamp

--- kill_rebuild.sh
+++ kill_rebuild.sh
@@ -4,7 +4,7 @@ docker ps -q -a | xargs docker stop
 docker ps -q -a | xargs docker rm
 docker rmi $(docker images)
 
-#docker-compose build
-#docker-compose up -d
+docker-compose build
+docker-compose up -d
 
 #docker-compose run web bash
